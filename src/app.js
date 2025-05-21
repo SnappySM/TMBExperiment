0dfa8704464663f6ec05e25638d33e85622cb2c3
SCOGNAMILLO Nicola <nicola.scognamillo@soprasteria.com>
2025-05-21 12:14:38 +0200

    Additional setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const app = express()
-const PORT = 3000;
+const PORT = 3000
 
 app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.status(200)
+    res.send("Welcome to root URL of Server")
 });
 
 app.listen(PORT, (error) => {
